4ef9cf6e76d084e0707b49fe60f05098709473a3
esesoves <esesoves@gmail.com>
2020-07-16 20:31:55 +0300

    add image galery

--- TravelSite/Controllers/AdminController.cs
+++ TravelSite/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -61,6 +62,91 @@ namespace TravelSite.Controllers
             db.Trips.Add(a);
             await db.SaveChangesAsync();
             return View();
+        }
+
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public IActionResult ShowImage()
+        {
+           var a =  db.ImageGalleries.ToList();
+            return View(a);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> CreateImage(ImageCreate entity)
+        {
+            ImageGallery person = new ImageGallery
+            {
+                Id = Guid.NewGuid(),
+                Country= entity.Country,
+                City =  entity.City,
+                Description = entity.Description,
+                Data = entity.Data
+
+            };
+            if (entity.Image != null)
+            {
+                byte[] imageData = null;
+                // считываем переданный файл в массив байтов
+                using (var binaryReader = new BinaryReader(entity.Image.OpenReadStream()))
+                {
+                    imageData = binaryReader.ReadBytes((int)entity.Image.Length);
+                }
+                // установка массива байтов
+                person.Image = imageData;
+            }
+            db.ImageGalleries.Add(person);
+            db.SaveChanges();
+
+            return View();
+        }
+
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> CreateImage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> EditImage(Guid? id)
+        {
+            if (id != null)
+            {
+                ImageGallery image = await db.ImageGalleries.FirstOrDefaultAsync(p => p.Id == id);
+                if (image != null)
+                    return View(image);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditImage(ImageGallery image)
+        {
+            db.ImageGalleries.Update(image);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "RequireAdministratorRole")]
+        public async Task<IActionResult> DeleteImage(Guid? id)
+        {
+            if (id != null)
+            {
+                ImageGallery trip = await db.ImageGalleries.FirstOrDefaultAsync(p => p.Id == id);
+
+                if (trip != null)
+                {
+                    var a = db.ImageGalleries.ToList();
+                    //a.Remove(trip);
+                    db.ImageGalleries.Remove(trip);
+                    await db.SaveChangesAsync();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
         }
     }
 }
--- TravelSite/Models/ImageGallery.cs
+++ TravelSite/Models/ImageGallery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace TravelSite.Models
 {
@@ -16,4 +17,17 @@ namespace TravelSite.Models
         public Guid TripId { get; set; }
         public byte[] Image { get; set; }
     }
+
+    public class ImageCreate
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public DateTime Data { get; set; }
+        public Guid TripId { get; set; }
+
+        public IFormFile Image { get; set; }
+    }
+
 }
